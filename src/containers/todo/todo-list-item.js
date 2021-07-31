a5880385aa465d15c846959499253c0a40587977
Javier Orrillo <javier.orrillo@nuorder.com>
2021-07-31 18:16:19 -0500

    TODO: mark as donde and delete

--- src/containers/todo/todo-list-item.js
+++ src/containers/todo/todo-list-item.js
@@ -1,10 +1,28 @@
 import React from "react";
+import styles from "./todo.module.scss";
+import classNames from "classnames";
 
-export const TodoListItem = ({ todo }) => {
+export const TodoListItem = ({ todo, onDelete, onMarkAsDone }) => {
+
+    // un closure que retorna el manejador del evento click
+    const handleDelete = (item) => {
+        return () => {
+            onDelete && onDelete(item);
+        };
+    };
+
+    const handleSelect = (item) => {
+        return () => {
+            onMarkAsDone && onMarkAsDone(item, !item.done);
+        };
+    };
+
 
     return (
-        <div>
-            {todo.title}
+        <div className={styles.todoItem}>
+            <input type="checkbox" checked={todo.done} onChange={handleSelect(todo)} />
+            <div  className={classNames(styles.title, { [styles.done]: todo.done })}>{todo.title}</div>
+            <input type="button" value="Eliminar" onClick={handleDelete(todo)} />
         </div>
     );
 };
--- src/containers/todo/todo-list.js
+++ src/containers/todo/todo-list.js
@@ -1,27 +1,54 @@
 import React from "react";
 import { TodoListItem } from "./todo-list-item";
 import { TodoListForm } from "./todo-list-form";
+import styles from "./todo.module.scss";
+import { v4 as uuid } from "uuid";
 
 export const TodoList = () => {
 
+    // getter y setter
     const [todoList, setTodoList] = React.useState([{
-        title: "tarea 1"
+        id: uuid(),
+        title: "tarea 1",
+        done: true,
+    },
+    {
+        id: uuid(),
+        title: "tarea 2",
+        done: false,
     }]);
 
     const handleAdd = (value) => {
         //  const newValues = [].concat(todoList, [{ title: value }]);
         //  setTodoList(newValues)
 
-        setTodoList(items => [].concat(items, [{ title: value }]))
+        setTodoList(items => [].concat(items, [{ id: uuid(), title: value, done: false }]));
+    };
+
+    const handleDelete = (item) => {
+        setTodoList(items => items.filter(it => it.id !== item.id));
+    }
+
+    const handleMarkAsDone = (item, isDone) => {
+        setTodoList(items => items.map(it => {
+            if (it.id === item.id) {
+                return {
+                    ...it,
+                    done: isDone,
+                };
+            }
+            return it;
+        }));
     }
 
     return (
         <div>
             <TodoListForm onAdd={handleAdd} />
-            {todoList.map((todo, index) => {
-                return (<TodoListItem key={index} todo={todo} />);}
-            )}
-            
+            <div className={styles.todoWrapper}>
+                {todoList.map((todo, index) => {
+                    return (<TodoListItem key={todo.id} todo={todo} onDelete={handleDelete} onMarkAsDone={handleMarkAsDone}  />);}
+                )}
+            </div>
         </div>
     )
 }
